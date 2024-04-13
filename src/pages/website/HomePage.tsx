4e4dfe94f4a9ffb5c166041b4139ed60ec125963
Ban Trieu <ban21032002@gmail.com>
2024-04-13 22:26:06 +0700

    .

--- src/pages/website/HomePage.tsx
+++ src/pages/website/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaRegClock, FaRegStar, FaStar } from "react-icons/fa";
+import { FaCalendarAlt, FaRegClock, FaRegStar, FaStar } from "react-icons/fa";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 
@@ -591,7 +591,7 @@ const HomePage = () => {
         </div>
       </div>
       {/* image */}
-      <div className="container flex mb-[20px]">
+      <div className="container flex pb-[30px]">
         <img
           src="https://demo.fieldthemes.com/bookshop/demo1/modules/fieldhtmlblock/images/banner21.jpg"
           alt=""
@@ -609,7 +609,7 @@ const HomePage = () => {
         </div>
       </div>
       {/* MOSTVIEW PRODUCTS */}
-      <div className="container flex items-center justify-between font-bold bg-[#f2f2f2] py-3 px-6 my-10">
+      <div className="container flex items-center justify-between font-bold bg-[#f2f2f2] py-3 px-6">
         <a className="text-[#444] hover:text-[#e5642f] cursor-pointer">
           MOSTVIEW PRODUCTS
         </a>
@@ -618,14 +618,360 @@ const HomePage = () => {
           <GrFormNext className="text-xl text-[#999] hover:text-[#e5642f]" />
         </div>
       </div>
-      <div className="container">
-        <img
-          src="https://demo.fieldthemes.com/bookshop/demo1/modules/fieldonecateproduct/views/img/banner_left.jpg"
-          alt=""
-        />
-
+      <div className="container flex pt-[20px]">
+        <div className="w-[30%] mr-12">
+          <img
+            src="https://demo.fieldthemes.com/bookshop/demo1/modules/fieldonecateproduct/views/img/banner_left.jpg"
+            alt=""
+            className="w-[265px] h-auto"
+          />
+        </div>
+        <div className="flex justify-between w-[100%]">
+          <div className=" w-[30%] border p-4">
+            <div className="flex pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" w-[30%] border p-4">
+            <div className="flex pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" w-[30%] border p-4">
+            <div className="flex pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-t-[1px] pt-4 pb-4">
+              <a href="">
+                <img
+                  src="https://demo.fieldthemes.com/bookshop/demo1/51-small_default/convallis-pharetra.jpg"
+                  alt=""
+                  className="border max-w-[100px] h-auto mr-[20px]"
+                />
+              </a>
+              <div className="m-auto text-[#666]">
+                <a href="" className="text-[12px] hover:text-[#e5642f]">
+                  Convallis Pharetra
+                </a>
+                <a href="" className="flex text-[12px]">
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                  <FaStar className="text-[#fc3] mr-[1px]" />
+                </a>
+                <div className="flex items-center justify-between my-2 pb-[9px] text-[12px]">
+                  <span className="text-[#e5642f] font-semibold">€31.31</span>
+                  <span className="line-through">€34.79</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
+      {/*  */}
+      <div className="container flex justify-between">
+        {/* OUR BLOG */}
+        <div className="container flex items-center justify-between font-bold bg-[#f2f2f2] py-3 px-6 w-[48%] !mx-0 !my-[20px]">
+          <a className="text-[#444] hover:text-[#e5642f] cursor-pointer">
+            OUR BLOG
+          </a>
+          <div className="flex">
+            <GrFormPrevious className="text-xl text-[#999] hover:text-[#e5642f]" />
+            <GrFormNext className="text-xl text-[#999] hover:text-[#e5642f]" />
+          </div>
+        </div>
+        {/* TOP BRANDS */}
+        <div className="container flex items-center justify-between font-bold bg-[#f2f2f2] py-3 px-6 w-[48%] !mx-0 !my-[20px]">
+          <a className="text-[#444] hover:text-[#e5642f] cursor-pointer">
+            TOP BRANDS
+          </a>
+          <div className="flex">
+            <GrFormPrevious className="text-xl text-[#999] hover:text-[#e5642f]" />
+            <GrFormNext className="text-xl text-[#999] hover:text-[#e5642f]" />
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className="container flex justify-between">
+        <div className="flex justify-between w-[48%]">
+          <div className="flex flex-col w-[48%] border">
+            <img
+              src="https://demo.fieldthemes.com/bookshop/demo1/modules/smartblog/images/4-home-default.jpg"
+              alt=""
+              className="w-[100%] hover:opacity-[0.7]"
+            />
+            <span className="text-[#444] text-[13px] px-5 pt-[15px]">
+              Vulputate Cursus
+            </span>
+            <div className="flex items-center text-[#e5642f] text-[13px] px-5 py-[4px]">
+              <FaCalendarAlt />
+              <span className="ml-2">Mar 07, 2024</span>
+            </div>
+            <span className="text-[12px] text-[#666] px-5 py-[2px] text-ellipsis">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys
+            </span>
+            <button className="m-5 border w-[30%] text-[13px] text-[#999] p-[2px] hover:bg-[#e5642f] hover:text-white">
+              Read more
+            </button>
+          </div>
+          <div className="flex flex-col w-[48%] border">
+            <img
+              src="https://demo.fieldthemes.com/bookshop/demo1/modules/smartblog/images/4-home-default.jpg"
+              alt=""
+              className="w-[100%] hover:opacity-[0.7]"
+            />
+            <span className="text-[#444] text-[13px] px-5 pt-[15px]">
+              Vulputate Cursus
+            </span>
+            <div className="flex items-center text-[#e5642f] text-[13px] px-5 py-[4px]">
+              <FaCalendarAlt />
+              <span className="ml-2">Mar 07, 2024</span>
+            </div>
+            <span className="text-[12px] text-[#666] px-5 py-[2px] text-ellipsis">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys
+            </span>
+            <button className="m-5 border w-[30%] text-[13px] text-[#999] p-[2px] hover:bg-[#e5642f] hover:text-white">
+              Read more
+            </button>
+          </div>
+        </div>
+        <div className="border">
+          <div className="flex border-b-[1px] border-gray-200">
+            <a href="" className="w-[33.3333%]">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+            <a href="" className="w-[33.3333%] border-x-[1px] border-gray-200">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+            <a href="" className="w-[33.3333%]">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="flex">
+            <a href="" className="w-[33.3333%]">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+            <a href="" className="w-[33.3333%] border-x-[1px] border-gray-200">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+            <a href="" className="w-[33.3333%]">
+              <img
+                src="https://demo.fieldthemes.com/bookshop/demo1/img/m/3-fieldbrand.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
